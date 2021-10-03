c1176fbba3f8822942c5263fbc8facecf09bf1d3
Octavius Bowman <octaviusbowman@gmail.com>
2021-10-03 19:07:44 -0400

    Initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Octavius Bowman on 10/3/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
